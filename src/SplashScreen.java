b6946cb17881d2827a203b21eb2e17b35a552eaa
Claire Luna Davis <clairelunadavis@gmail.com>
2024-08-14 12:27:25 +1000

    REQ-7: Change tab to 2 spaces, add group and creator name to splash screen

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -11,10 +11,11 @@ public class SplashScreen extends JWindow {
 
   public void showSplash() {
     JPanel content = (JPanel) getContentPane();
+    JPanel textPanel = new JPanel(new BorderLayout());
     content.setBackground(Color.white);
 
     int width = 450;
-    int height = 700;
+    int height = 550;
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (screenSize.width - width) / 2;
     int y = (screenSize.height - height) / 2;
@@ -22,10 +23,15 @@ public class SplashScreen extends JWindow {
 
     JLabel label = new JLabel(new ImageIcon("src/resources/_c523d561-f13f-46b3-8923-82dcad276dfd.jpg"));
     JLabel copyrt = new JLabel("2024, 3815ICT");
+    JLabel group = new JLabel("Group 8");
+    JLabel creator = new JLabel("Claire Luna Davis");
     copyrt.setFont(new Font("Sans-Serif", Font.BOLD, 15));
 
     content.add(label, BorderLayout.CENTER);
-    content.add(copyrt, BorderLayout.SOUTH);
+    textPanel.add(copyrt, BorderLayout.NORTH);
+    textPanel.add(group, BorderLayout.CENTER);
+    textPanel.add(creator, BorderLayout.SOUTH);
+    content.add(textPanel, BorderLayout.SOUTH);
 
     Color oraRed = new Color(156, 20, 20, 255);
     content.setBorder(BorderFactory.createLineBorder(oraRed, 10));
